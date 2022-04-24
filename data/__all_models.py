2253e10f1ea9fca417199d8b5664160a99d5c326
elizawetta <lari.x@mail.ru>
2022-04-25 00:06:52 +1000

    Add files via upload

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,15 +1,21 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, EmailField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = "User"
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 primary_key=True, unique=True, nullable=False)
     login = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
     hash_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     salt = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
 
 
 class Passwords(SqlAlchemyBase):
@@ -19,3 +25,9 @@ class Passwords(SqlAlchemyBase):
     user_id = sqlalchemy.Column(sqlalchemy.Integer, unique=True, nullable=False)
     cite = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+
+class LoginForm(FlaskForm):
+    login = EmailField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
